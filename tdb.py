a040226f6e0686c639687c5212e41fc4c98c0089
B-a-S <bas@netgroup.ua>
2017-11-14 13:32:30 +0200

    Add files via upload

---
+++ tdb.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import tdb
+import sys
+import time
+
+while 1==1:
+    db=tdb.open(sys.argv[1])
+    h=c=0
+
+
+# traverse records
+    for key in db:
+    if key[0:4]=='pppd':
+        ls=db[key].split(";")
+        if len(ls)<8:
+	h=h+1
+        else:
+	c=c+1
+
+    sys.stdout.write("\rhalted %4d, connected %4d" % (h,c))
+    sys.stdout.flush()
+    db.close()
+    time.sleep(3)
