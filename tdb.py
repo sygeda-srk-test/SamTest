6298c9c0215adae94bad68d2561dfd2f027a6961
B-a-S <bas@netgroup.ua>
2017-11-14 19:00:04 +0200

    Nest

--- tdb.py
+++ tdb.py
@@ -7,17 +7,16 @@ import time
 while 1 == 1:
     db = tdb.open(sys.argv[1])
     h = c = 0
-    
+
     for key in db:
         if key[0:4] == 'pppd':
-                ls = db[key].split(";")
-                if len(ls) < 8:
-                        h = h+1
-                else:
-                        c = c+1
-    
+		ls = db[key].split(";")
+		if len(ls) < 8:
+			h = h+1
+		else:
+			c = c+1
+
     sys.stdout.write( "\rhalted %4d, connected %4d" % ( h, c ))
     sys.stdout.flush()
     db.close()
     time.sleep(3)
-    
