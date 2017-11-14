3064db08e2ddb19a9b1903fb30f84367edd20af9
B-a-S <bas@netgroup.ua>
2017-11-14 14:49:09 +0200

    Fourth

--- tdb.py
+++ tdb.py
@@ -10,14 +10,13 @@ while 1==1:
 
 
 # traverse records
-    for key in db:
-	if key[0:4]=='pppd':
+	for key in db:
+	    if key[0:4]=='pppd':
     		ls=db[key].split(";")
     		if len(ls)<8:
     	    	    h = h+1
     		else:
 		c = c+1
-
     sys.stdout.write("\rhalted %4d, connected %4d" % (h,c))
     sys.stdout.flush()
     db.close()
