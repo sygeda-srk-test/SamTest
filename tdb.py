1a7af7e464baa49aa826bebe6d484ec530881297
B-a-S <bas@netgroup.ua>
2017-11-14 15:30:31 +0200

    comit

--- tdb.py
+++ tdb.py
@@ -9,14 +9,14 @@ while 1 == 1:
     h = c = 0
     
     for key in db:
-	    if key[0:4] == 'pppd':
+        if key[0:4] == 'pppd':
     		ls = db[key].split(";")
     		if len(ls) < 8:
     	    	    h = h+1
     		else:
 		    c = c+1
     
-    sys.stdout.write("\rhalted %4d, connected %4d" % (h , c))
+    sys.stdout.write("\rhalted %4d, connected %4d" % ( h, c ))
     sys.stdout.flush()
     db.close()
     time.sleep(3)
